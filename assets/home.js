cdda873acc02276db62ff455f86158877fbca81e
HMan <ralaitsiravasoloherimandranto@gmail.com>
2022-07-31 00:28:31 +0200

    création d'entité,controlleur,service,fonctionnement ajax

---
+++ assets/home.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('#shortenForm');
+const shortenCard = document.querySelector('#shortenCard');
+const inputUrl = document.querySelector('#url');
+const btnShorten = document.querySelector('#btnShortenUrl');
+
+
+const URL_SHORTEN = '/ajax/shorten';
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    fetch(URL_SHORTEN, {
+        method: 'POST',
+        body: new FormData(e.target)
+    })
+        .then(response => response.json())
+        .then(handleData)
+
+})
+
+const handleData = function (data) {
+    console.log(data);
+}
